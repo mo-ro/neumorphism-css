a82f6a087e2f31167c8b6ac097beb2900198776c
mo-ro <gainmoro@gmail.com>
2020-07-16 20:08:24 +0900

    set up scss env

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,17 @@
+const { resolve } = require('path')
+
 module.exports = {
   stories: ['../stories/**/*.stories.js'],
-  // webpackFinal: async(config) => {
-  //   config.module.rules.push({
-  //     test: /\.scss$/,
-  //     use: [
-  //       'style-loader',
-  //       'css-loader',
-  //       'sass-loader'
-  //     ],
-  //     include: path.resolve(__dirname, '../'),
-  //   })
-  //   return config
-  // },
-};
+  webpackFinal: async(config, {configType}) => {
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: [
+        'style-loader',
+        'css-loader',
+        'sass-loader'
+      ],
+      include: resolve(__dirname, '../'),
+    })
+    return config
+  }
+}
