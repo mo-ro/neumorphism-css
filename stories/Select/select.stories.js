a94b55eb8cb408f3dc8b5f9d2950e8a5541c24e1
mo-ro <gainmoro@gmail.com>
2020-07-23 17:49:08 +0900

    add selectbox component

---
+++ stories/Select/select.stories.js
@@ -0,0 +1,6 @@
+import {storiesOf} from '@storybook/html';
+
+import Select from './select.template';
+
+storiesOf('Select', module)
+  .add('Select', () => Select());
---
+++ stories/Select/select.template.js
@@ -0,0 +1,9 @@
+export default () => {
+  return `
+    <select name="" class="n-select">
+      <option value="">aaa</option>
+      <option value="">bbb</option>
+      <option value="">ccc</option>
+    </select>
+  `;
+}
