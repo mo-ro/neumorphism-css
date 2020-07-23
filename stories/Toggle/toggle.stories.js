19f526980eef05ebc092c7de313e287d7eed8511
mo-ro <gainmoro@gmail.com>
2020-07-23 16:42:31 +0900

    add toggle button component

---
+++ stories/Toggle/toggle.stories.js
@@ -0,0 +1,6 @@
+import {storiesOf} from '@storybook/html';
+
+import Toggle from './toggle.template';
+
+storiesOf('Toggle', module)
+  .add('Toggle', () => Toggle());
---
+++ stories/Toggle/toggle.template.js
@@ -0,0 +1,10 @@
+export default () => {
+  return `
+    <div class="part -toggle">
+      <label>
+        <input type="checkbox" class="n-toggle">
+        <div class=""></div>
+      </label>
+    </div>
+  `
+}
