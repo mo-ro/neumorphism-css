74dba6f59997fc42e5a9901e3e8c3462015ecda2
mo-ro <gainmoro@gmail.com>
2020-08-13 17:11:45 +0900

    add Tab component

---
+++ stories/Tab/tab.stories.js
@@ -0,0 +1,6 @@
+import {storiesOf} from '@storybook/html';
+
+import Tab from './tab.template';
+
+storiesOf('Tab', module)
+  .add('Tab', () => Tab());
---
+++ stories/Tab/tab.template.js
@@ -0,0 +1,8 @@
+export default () => {
+  return `
+    <div class="n-tab">
+      <button class="-active">Tab A</button>
+      <button>Tab B</button>
+    </div>
+  `;
+}
